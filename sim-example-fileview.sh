e96435ea1778c32808552888165736ee874800f4
Bjørnar <598062@stud.hvl.no>
2023-09-22 00:04:39 +0200

    endret shell script til å bruke bash
    sørger for at de som skal sjekke koden kommer til å klare å kjøre skriptet

--- sim-example-fileview.sh
+++ sim-example-fileview.sh
@@ -1,8 +1,9 @@
-#!/bin/zsh
+#!/bin/bash
 
 if [[ -d "./example-process-view" ]]; then
   echo "example-process-view directory exists, continuing..." 
   echo "removing old existing log files..."
+  # trenger ikke se output fra rm kommando, sender stdout til /dev/null
   rm ./example-process-view/* > /dev/null
 else 
   mkdir ./example-process-view
@@ -13,11 +14,13 @@ info_regex='T=([0-9]+) Scheduled: T([0-9]+) Ready: (T[0-9]+(, T[0-9]+)*)?'
 ./gradlew run | while IFS= read -r line; do
   echo $line 
   if [[ $line =~ $info_regex ]]; then
-    # hvis en bruker bash istedenfor zsh så erstatt match med BASH_REMATCH.
-    # match og BASH_REMATCH er innebygde variabler som holder en liste med
-    # alle captured grupper fra regex matching med if [[ string =~ regex ]]
-    #echo $match
-    filename="T${match[2]}-proc.log"
+    # BASH_REMATCH er en innebygget variabel som blir oppdatert med matchede
+    # grupper fra REGEX matching med innebygget [[ string =~ regex_pattern ]]
+    # metode for regex matching.
+    # regex pattern måtte omskrives for å fungere med denne regex matcheren
+    # fikk ikke de oppgitte sed kommandoene og regex patterns til å funke, 
+    # så måtte bruke et annet alternativ
+    filename="T${BASH_REMATCH[2]}-proc.log"
     echo "$filename"
     if [ ! -e ./example-process-view/$filename ]; then
       touch "./example-process-view/$filename"
